551a5634eab7f6d6625824eb96430a617bf51d80
Johannes Borgström <johannes@highspirits.se>
2017-04-24 11:03:03 +0200

    Following an invitation link will log out any logged in members and remember the invited member

--- app/controllers/invitations_controller.rb
+++ app/controllers/invitations_controller.rb
@@ -1,12 +1,28 @@
 class InvitationsController < ApplicationController
 
+  # Member clicked on invitation link, thereby accepting the invitation
+  # /invitations/<token>/edit?email=<url escaped email address>
   def edit
     member = Member.find_by(email: params[:email])
-    if member && !member.activated? && member.authenticated?(:invitation, params[:id])
-      member.update_attribute(:activated, true)
+    # Check if someone is logged in already
+    # If logged in member differs from the invited member,
+    # log out current member and log 
+    if current_member && !current_member?(member) {
+      log_out
+      puts "Logging out #{current_member.name}."
+    }
+    if member && member.authenticated?(:invitation, params[:id])
+      message = "Welcome #{member.name}!"
+      puts "Invitation accepted by '#{member.name}, <#{member.email}>'"
+      # Activate member if not already activated
+      unless member.activated?
+        member.update_attribute(:activated, true)
+        message += "Your account has been activated."
+        puts "Account activated for '#{member.name}, <#{member.email}>'"
+      end
       log_in member
-      flash[:success] = "Account activated!"
-      puts "Account activated for #{member.email}"
+      remember member
+      flash[:success] = message
     end
     redirect_to root_url
   end
--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -1,19 +1,24 @@
 class MembersController < ApplicationController
 
   before_action :logged_in_member
-  before_action :must_be_admin, only: [:new, :create, :destroy]
+  before_action :must_be_admin, only: [:new, :index, :create, :destroy]
 
+  # ADMIN ONLY:
+  # List all members. Edit or Delete.
   # /members
   def index
     @members = Member.all
   end
 
+  # ADMIN ONLY:
+  # Create new member
   # /members/new
   def new
     @member = Member.new
   end
 
-  # Create Member and send invitation email.
+  # ADMIN ONLY:
+  # Creating a new Member, saves to db and sends invitation email.
   # post /members
   def create
     @member = Member.new(member_params)
