7b4a94c65fc8b190218e039afabffca38f8b938b
Johannes Borgström <johannes@highspirits.se>
2017-04-25 22:34:08 +0200

    Add activerecord-import gem. Create multiple transactions at once

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -1,5 +1,8 @@
 class TransactionsController < ApplicationController
 
+  before_action :logged_in_member
+  before_action :must_be_admin, only: [:new, :index, :create, :create_many, :destroy]
+
   def new
     @transaction = Transaction.new
   end
@@ -11,6 +14,14 @@ class TransactionsController < ApplicationController
     else
       puts "couldn't save"
     end
+  end
+
+  def create_many
+    @transactions = []
+    params[:transactions].each do |transaction|
+      @transactions << Transaction.new(transaction.require(:transaction).permit(:message, :amount, :date))
+    end
+    Transaction.import @transactions
   end
 
   def index
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   post    '/login',   to: 'sessions#create'
   delete  '/logout',  to: 'sessions#destroy'
 
+  post    '/transactions/many', to: 'transactions#create_many'
+
   resources :members
   resources :transactions
   resources :invitations, only: [:edit]
