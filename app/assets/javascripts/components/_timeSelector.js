d5649bb2a03887a221d5663ce9a6f36e72f7c5b9
Johannes Borgström <johannes.borgstrom@tretton37.com>
2017-10-02 15:05:48 +0200

    Simplify member color assignment in Calendar

--- app/assets/javascripts/components/_timeSelector.js
+++ app/assets/javascripts/components/_timeSelector.js
@@ -545,13 +545,6 @@ TimeSelector.prototype.pointFromEvent = function(e) {
   }
 }
 /**
-*   Calculates the next frame in preparation for drawing
-*/
-TimeSelector.prototype.prepare = function() {
-//  this.drawHourPie(17, 22);
-}
-
-/**
 *   Updates canvas by clearing and then drawing to it.
 */
 TimeSelector.prototype.draw = function() {
@@ -559,8 +552,6 @@ TimeSelector.prototype.draw = function() {
   this.clear();
   // Draw background
   this.drawBackground();
-  // Prepare for drawing
-  this.prepare();
   var info = "";
   // Draw previous bookings if any
   this.selectedHours.bookings.forEach(function(booking) {
